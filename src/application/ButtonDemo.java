5308299e81689402577c152594dcab7eb390fd7c
Florent K <komlavi.elom@outlook.fr>
2025-02-04 16:47:48 -0500

    first commit

--- src/application/ButtonDemo.java
+++ src/application/ButtonDemo.java
@@ -1,16 +1,6 @@
 package application;
-
-import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.stage.Stage;
-
+import javafx.application.Application;
 public class ButtonDemo extends Application{
 	public static void main(String[] args) {
 		launch(args);
